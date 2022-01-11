a395834065fd82940a9f0310ee124ea90142eb06
MQiLEE <95162273+MQiLEE@users.noreply.github.com>
2022-01-11 19:13:31 +0800

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -11,8 +11,9 @@ $dbname="db_qms";
 $con = mysqli_connect($servername, $username, $password, $dbname);
 
 //Check connection
-
-//Close connection
+if ($con->connect_error) {
+      die("Connection failed: " . $con->connect_error);
+    }
 
 
 
